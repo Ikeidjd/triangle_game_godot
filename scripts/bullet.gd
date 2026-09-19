0f8fab98ff1090094ee64b59809b79764cbce07c
Ikeidjd <ikeidjd2@gmail.com>
2026-09-19 03:01:41 +0200

    Added bullets

---
+++ scripts/bullet.gd
@@ -0,0 +1,6 @@
+class_name Bullet
+extends RigidBody2D
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	queue_free()
--- scripts/player.gd
+++ scripts/player.gd
@@ -2,6 +2,9 @@ class_name Player
 extends CharacterBody2D
 
 @onready var smooth_mover: SmoothMover = %SmoothMover
+@onready var bullet_cooldown: Timer = %BulletCooldown
+@export var bullet_scene: PackedScene
+@export var bullet_speed: float
 
 
 func _physics_process(delta: float) -> void:
@@ -19,3 +22,10 @@ func _physics_process(delta: float) -> void:
 	velocity = smooth_mover.get_new_velocity(delta, velocity, rotation, dir)
 	move_and_slide()
 	rotation = smooth_mover.get_new_rotation(velocity, rotation)
+	
+	if Input.is_action_just_pressed("shoot") and bullet_cooldown.is_stopped():
+		var bullet: Bullet = bullet_scene.instantiate()
+		bullet.position = position
+		bullet.linear_velocity = (get_global_mouse_position() - position).normalized() * bullet_speed
+		get_tree().root.add_child(bullet)
+		bullet_cooldown.start()
